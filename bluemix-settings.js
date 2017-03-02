3837aedb6143a0f90e5b52cfc575b2bed14ab941
Johnson Chiang <chiangys@tw.ibm.com>
2017-03-01 18:20:47 -0600

    Add 'crc' module to node-red function node

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -49,7 +49,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+    	crc: require('crc')
+    },
 
     storageModule: require("./couchstorage")
 }
